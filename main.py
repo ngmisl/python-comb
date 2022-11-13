4e24fb6acc0e09be57c0132901c2f10ca6291c7a
ngmisl <98217124+ngmisl@users.noreply.github.com>
2022-11-13 10:35:48 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import math
+
+# player must select 2 skills
+skills = ["Farming", "Melee", "Range", "Crafting", "Fishing", "Brewing"]
+
+
+def main():
+    num_skills: int = len(skills)
+    variations: int = math.comb(num_skills, 2)
+    print(variations)
+
+
+if __name__ == "__main__":
+    main()
